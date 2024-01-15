90342d9f1e631bae141f44a28a853b0214b53fd3
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2024-01-15 11:55:44 -0500

    Add files via upload

---
+++ ls.bat
@@ -0,0 +1,5 @@
+@echo off
+
+for /d %%F in (*) do echo [36m%%F[0m
+
+for %%i in (*) do echo [35m%%i[0m
---
+++ setJavaVer.bat
@@ -0,0 +1,4 @@
+@echo off
+if %1==17 setx JAVA_HOME "C:\Program Files\Java\jdk-17" /m
+if %1==8 setx JAVA_HOME "C:\Program Files\Java\jre-1.8" /m
+if %1==20 setx JAVA_HOME "C:\Program Files\Java\jdk-20.0.2" /m
