8f7a0a5afd89045e0040e98912e95153c7ebf8df
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2024-01-15 12:05:09 -0500

    Comment setJavaVer.bat

--- setJavaVer.bat
+++ setJavaVer.bat
@@ -6,3 +6,5 @@ Rem if %1==<JavaVersion> setx JAVA_HOME <Path to JavaVersion> /m
 if %1==17 setx JAVA_HOME "C:\Program Files\Java\jdk-17" /m
 if %1==8 setx JAVA_HOME "C:\Program Files\Java\jre-1.8" /m
 if %1==20 setx JAVA_HOME "C:\Program Files\Java\jdk-20.0.2" /m
+
+Rem the changes may not be instant, make sure to refresh/close and reopen cmd
