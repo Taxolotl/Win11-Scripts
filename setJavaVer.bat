ac8f811a346ca27d9fc15dd66d007cff5bdfd8e0
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2024-01-15 12:03:52 -0500

    Update setJavaVer.bat

--- setJavaVer.bat
+++ setJavaVer.bat
@@ -1,4 +1,8 @@
 @echo off
+
+Rem this is just to change the default java version if you have multiple installed. the structure is
+Rem if %1==<JavaVersion> setx JAVA_HOME <Path to JavaVersion> /m
+
 if %1==17 setx JAVA_HOME "C:\Program Files\Java\jdk-17" /m
 if %1==8 setx JAVA_HOME "C:\Program Files\Java\jre-1.8" /m
 if %1==20 setx JAVA_HOME "C:\Program Files\Java\jdk-20.0.2" /m
