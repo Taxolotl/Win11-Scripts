1282f7afdbc3418fed4d0b99d3a34876730053d1
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2024-01-15 12:06:21 -0500

    Update ls.bat

--- ls.bat
+++ ls.bat
@@ -1,5 +1,6 @@
 @echo off
 
-for /d %%F in (*) do echo %%F Rem My original script had additional stuff around %%F to change the color, but github doesn't like the escape character (ascii code 27)
+Rem My original script had additional stuff around %%F to change the color, but github doesn't like the escape character (ascii code 27)
 
+for /d %%F in (*) do echo %%F 
 for %%i in (*) do echo %%i
